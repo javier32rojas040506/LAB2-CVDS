776e0986af3b5161ec2f854b0a8b68abdeca7b6c
Francico Javier Rojass <javier32rojas040506@gmail.com>
2022-02-02 09:42:38 -0500

    Se personaliza Saludo con nombres

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello " + args[0] + " " + args[1] + " " + args[2]);
+    }
+}
